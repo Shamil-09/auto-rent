ceed5963206dcef688f4fae11b5e03e90a9af3ca
Shamil <Shamil@Shamils-MacBook-Pro.local>
2020-06-02 23:06:09 -0500

    feat: added about and auto sections

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+
+
+
+
+});
