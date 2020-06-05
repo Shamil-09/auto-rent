73181becc47a4662fb416dde8057e61ad76f62a5
Shamil <Shamil@Shamils-MacBook-Pro.local>
2020-06-04 21:39:53 -0500

    feat: connected nav links

--- js/main.js
+++ js/main.js
@@ -10,7 +10,34 @@ $(document).ready(function () {
     },
   });
 
+  // Smooth scrolling
 
+  $('.go-to').click(function () { // ловим клик по ссылке с классом go_to
+    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+    }
+    return false; // выключаем стандартное действие
+  });
+
+  // Youtube
+
+  var player;
+  $('.hero__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '465',
+      width: '100%',
+      videoId: 'BkGFhBypWrc',
+      events: {
+        'onReady': videoPlay,
+
+      }
+    });
+  })
+
+  function videoPlay(event) {
+    event.target.playVideo();
+  }
 
 
 
